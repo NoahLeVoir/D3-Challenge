c0cf8d6edab79c158aff9c3a26bc7c6ce25b6cd8
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-04-25 18:09:22 -0500

    Create svg element, parse csv data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,39 @@
 // Make sure app.js is loaded and connected to index.html
 console.log("app.js loaded")
 
+
+// Define svg area for eventual chart
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
 // Load the csv data using d3.csv
 d3.csv("assets/data/data.csv").then(csvData => {
     console.log("csv data has been loaded");
     console.log(csvData);
 
+    // Parse the data
+    csvData.forEach(function(csvData) {
+        csvData.poverty = +csvData.poverty;
+        csvData.age = +csvData.age;
+        csvData.income = +csvData.income;
+        csvData.healthcare = +csvData.healthcare;
+        csvData.obesity = +csvData.obesity;
+        csvData.smokes = +csvData.smokes;
+        });
 
-    var resultArray = csvData.filter(s => s);
-    var result = resultArray[0];
-    console.log(resultArray);
-
-    // Define variables to be used for statics csv
-    var smoker = result.smokes 
-    var age = result.age
-    
-    console.log(smoker);
-    console.log(age);
 
 });
-
-// Create a static scatter plot shell
-
-
-// Create and situate axes labels to the left and bottom of the chart
 
 // Include state abbreviations in the scatter plot circles
 

ab74610915ae93d336fb7b47eb38160ef4e4e27e
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-04-25 14:35:12 -0500

    Set up initial requirement sections with comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-// @TODO: YOUR CODE HERE!
+// Load the csv data using d3.csv
+
+// Create a static scatter plot shell
+
+// Create and situate axes labels to the left and bottom of the chart
+
+// Include state abbreviations in the scatter plot circles
+
+// Populate the scatter plot with two variables
+// Healthcare v. Poverty or Smokers v. Age
+
+// BONUS
+// Place additional labels and a click event
+// So that the user can pick what data to display, animate the transition
+// Incorpare d3-tip (Use d3-tip.js plugin)
